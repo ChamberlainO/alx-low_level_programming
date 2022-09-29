7de951625b9714f559fc186d40d8ab40087d3001
ChamberlainO <omuchamberlain@gmail.com>
2022-09-29 14:19:11 -0700

    Inception

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -10,6 +11,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _putchar(char c);
+int _sqrt(int n, int x);
+int is_prime(int n, int start);
 
 #endif /*MAIN_H*/
