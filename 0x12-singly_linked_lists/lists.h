77b2b900bdd86afcd0df03e984965bc763986b6f
ChamberlainO <omuchamberlain@gmail.com>
2022-10-20 23:18:00 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,13 +7,13 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @str: string - (malloc string)
  * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
+
 typedef struct list_s
 {
 	char *str;
