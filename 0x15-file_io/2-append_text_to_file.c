7f9ec57370475beebb73855f31ff604a728c44a4
ChamberlainO <omuchamberlain@gmail.com>
2022-10-31 16:21:09 -0700

    cp

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,28 +11,27 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, x, len = 0;
+	int fdo, fdw, len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
 
+	fdo = open(filename, O_RDWR | O_APPEND);
+	if (fdo < 0)
+		return (-1);
 	if (text_content == NULL)
 	{
-		close (fd);
+		close(fdo);
 		return (1);
 	}
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	x = write(fd, text_content, len);
+	while (*(text_content + len))
+		len++;
 
-	if (fd == -1 || x == -1)
-	{
+	fdw = write(fdo, text_content, len);
+	close(fdo);
+	if (fdw < 0)
 		return (-1);
-	}
-
-	close(fd);
 
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include <sys/uio.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #include <stdlib.h>
